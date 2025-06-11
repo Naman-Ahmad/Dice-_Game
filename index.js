7747663a3f58895144ed5c538a7fd0b9a166f7c2
Naman-Ahmad <namanahmad404@gmail.com>
2025-06-11 15:09:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// Generate random numbers between 1 and 6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+// Set dice images
+document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+
+// Display result
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player One Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player Two Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "It's a Draw!";
+}
